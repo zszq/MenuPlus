49c3684c78e547c0542445dfdbe87f087e52db70
zl <867309686@qq.com>
2026-05-12 00:27:57 +0800

    Initial Commit

---
+++ rightMenu/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  rightMenu
+//
+//  Created by l z on 2026/5/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ rightMenu/rightMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  rightMenuApp.swift
+//  rightMenu
+//
+//  Created by l z on 2026/5/12.
+//
+
+import SwiftUI
+
+@main
+struct rightMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
